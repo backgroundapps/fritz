c7983522192c35e5f45070b799daaddbf05b74db
Jonatas Emidio <jonatasemidio@gmail.com>
2023-02-27 15:02:11 +0000

    adding pws

--- fritz-app/routes/index.tsx
+++ fritz-app/routes/index.tsx
@@ -1,11 +1,22 @@
 import { Head } from "$fresh/runtime.ts";
 import Counter from "../islands/Counter.tsx";
 
+
 export default function Home() {
   return (
     <>
       <Head>
         <title>Fresh App</title>
+        <link
+          crossorigin="use-credentials"
+          rel="manifest"
+          href="/manifest.webmanifest"
+        />
+        <script type="module">
+          import "https://cdn.jsdelivr.net/npm/@pwabuilder/pwaupdate/dist/pwa-update.js";
+          const el = document.createElement("pwa-update");
+          document.body.appendChild(el);
+        </script>
       </Head>
       <div class="p-4 mx-auto max-w-screen-md">
         <img
